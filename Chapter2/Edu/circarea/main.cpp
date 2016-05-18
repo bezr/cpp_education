5b6b5f105542fee67f891bf7fd5dc00d0735c32d
bezr <bezr41@gmail.com>
2016-05-19 01:32:23 +0300

    Include iomanip using setw()

--- Chapter2/Edu/circarea/main.cpp
+++ Chapter2/Edu/circarea/main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    float rad;
+    const float PI = 3.14159F;
+    cout << "Input radius of circle:\n";
+    cin >> rad;
+    float area = PI * rad * rad;
+    cout << "Area of circle = " << area << endl;
+
     return 0;
 }
