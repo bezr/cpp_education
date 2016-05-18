5fd6a8984044145faced3a3d474cf15aa3d4dc06
bezr <bezr41@gmail.com>
2016-05-19 01:17:29 +0300

    Forgot to save before commit

--- Chapter2/Edu/fahren/main.cpp
+++ Chapter2/Edu/fahren/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    int ftemp;
+    cout << "Input temerature in farphen" << endl;
+    cin >> ftemp;
+    int ctemp = (ftemp - 32) * 35 / 9;
+    cout << "Temeprature in celcius  = ";
+    cout <<ctemp << endl;
     return 0;
 }
